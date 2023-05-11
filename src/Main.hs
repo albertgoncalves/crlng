d6fff2f77770ef7fdadd13d81a64672d6cc4ad8f
albertgoncalves <goncalves.albert@gmail.com>
2023-05-10 21:14:22 -0400

    control `stderr` printing via CPP

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE CPP #-}
+
+#define VERBOSE 1
+
 import Compile (compile)
 import Data.ByteString.Builder (toLazyByteString)
 import qualified Data.ByteString.Lazy as B
 import Parse (parse)
 import System.Environment (getArgs)
+
+#if VERBOSE
 import System.IO (hPrint, stderr)
+#endif
 
 main :: IO ()
 main = do
   [pathIn, pathOut] <- getArgs
   funcs <- parse <$> readFile pathIn
+#if VERBOSE
   mapM_ (hPrint stderr) funcs
+#endif
   B.writeFile pathOut $ toLazyByteString $ compile funcs
